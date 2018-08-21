9c57943eea2718ac26a6805d945f9bdcb1cddd70
Ally <agraham@scottishpower.com>
2018-08-21 16:25:58 +0100

    Some work transforming teams.

--- function.js
+++ function.js
@@ -16,38 +16,52 @@ function sortTableOrder(league) {
 }
 
 function buildTableFromResults(results) {
-  let unorderedTable = {};
+  const teams = {};
 
-  results.rounds.map(
-    round => round.matches.map(
+  results.rounds.forEach(
+    round => round.matches.forEach(
       match => {
-        unorderedTable.key = match.team1.key;
-        unorderedTable.name = match.team1.name;
-        unorderedTable.code = match.team1.code;
-        unorderedTable.wins = match.score1 > match.score2 ?  1 : 0;
-        unorderedTable.draws = match.score1 === match.score2 ? 1 : 0;
-        unorderedTable.loses = match.score1 < match.score2 ? 1 : 0;
-        unorderedTable.goals_for = match.score1;
-        unorderedTable.goals_against = match.score2;
-        unorderedTable.goal_difference = match.score1 - match.score2;
-        unorderedTable.points = match.score1 === match.score2 ? 1 : match.score1 > match.score2 ? 3 : 0;
+        let team1 = teams[match.team1.key];
+        if (!team1) {
+          team1 = {
+            key: match.team1.key,
+            name: match.team1.name,
+            code: match.team1.code,
+            wins: match.score1 > match.score2 ?  1 : 0,
+            draws: match.score1 === match.score2 ? 1 : 0,
+            loses: match.score1 < match.score2 ? 1 : 0,
+            goals_for: match.score1,
+            goals_against: match.score2,
+            goal_difference: match.score1 - match.score2,
+            points: match.score1 === match.score2 ? 1 : match.score1 > match.score2 ? 3 : 0
+          };
+          teams[match.team1.key] = team1;
+        } else {
+          
+        }
 
-        unorderedTable.key = match.team2.key;
-        unorderedTable.name = match.team2.name;
-        unorderedTable.code = match.team2.code;
-        unorderedTable.wins = match.score2 > match.score1 ?  unorderedTable.wins + 1 : unorderedTable.wins;
-        unorderedTable.draws = match.score2 === match.score1 ? 1 : 0;
-        unorderedTable.loses = match.score2 < match.score1 ? 1 : 0;
-        unorderedTable.goals_for = match.score2;
-        unorderedTable.goals_against = match.score1;
-        unorderedTable.goal_difference = match.score2 - match.score1;
-        unorderedTable.points = match.score1 === match.score2 ? 1 : match.score2 > match.score1 ? 3 : 0
-        console.log(unorderedTable);
+        let team2 = teams[match.team2.key];
+        if (!team2) {
+          team2 = {
+            key: match.team2.key,
+            name: match.team2.name,
+            code: match.team2.code,
+            wins: match.score2 > match.score1 ? 1 : 0,
+            draws: match.score2 === match.score1 ? 1 : 0,
+            loses: match.score2 < match.score1 ? 1 : 0,
+            goals_for: match.score2,
+            goals_against: match.score1,
+            goal_difference: match.score2 - match.score1,
+            points: match.score1 === match.score2 ? 1 : match.score2 > match.score1 ? 3 : 0
+          }
+        }
       }
     )
   )
 
-  return sortTableOrder(unorderedTable);
+  return sortTableOrder(teams);
 }
 
-buildTableFromResults(mockData);
+const results = buildTableFromResults(mockData);
+
+console.log(results);

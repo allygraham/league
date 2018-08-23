341a3eb9541e878e07326207ca193b1a012a159a
Ally Graham <ally@allygraham.co.uk>
2018-08-23 22:24:01 +0100

    Added actual json. Refactored team logic into separate functions. Added sort function. Added rank to teams.

--- function.js
+++ function.js
@@ -1,80 +1,81 @@
-const mockData = require('./mock');
+// const data = require('./mock');
+const data = require('./en.1.json');
 
-function sortTableOrder(league) {
-  // use slice() to copy the array and not just make a reference
-  // var byPoints = league.slice(0);
-  //   byPoints.sort(function(a,b) {
-  //   return a.points - b.points;
-  // });
+const teams = [];
 
-  // var byGoalDifference = byPoints.slice(0);
-  //   byGoalDifference.sort(function(a,b) {
-  //   return a.goal_difference - b.goal_difference;
-  // });
+function addTeamToTable(match, team) {
+  teams.push({
+    key: team === 1 ? match.team1.key : match.team2.key,
+    name: team === 1 ? match.team1.name : match.team2.key,
+    code: team === 1 ? match.team1.code : match.team2.code,
+    wins: match.score1 > match.score2 && team === 1 ? 1 : 0,
+    draws: match.score1 === match.score2 ? 1 : 0,
+    loses: match.score1 < match.score2 && team === 1 ? 1 : 0,
+    goals_for: team === 1 ? match.score1 : match.score2,
+    goals_against: team === 1 ? match.score2 : match.score1,
+    goal_difference: team === 1 ? match.score1 - match.score2 : match.score2 - match.score1,
+    points: match.score1 === match.score2 ? 1 : 
+      (match.score1 > match.score2 && team === 1) || (match.score1 < match.score2 && team === 2) ? 3 : 0
+  });
+}
 
-  // console.log(byGoalDifference);
+function updateTeam(match, teamKey, team) {
+  const teamToUpdate = teams.findIndex((team => team.key === teamKey));
+  teams[teamToUpdate].wins = match.score1 > match.score2 && team === 1 ? teams[teamToUpdate].wins + 1 : teams[teamToUpdate].wins;
+  teams[teamToUpdate].draws = match.score1 === match.score2 ? teams[teamToUpdate].draws + 1 : teams[teamToUpdate].draws;
+  teams[teamToUpdate].loses = match.score1 < match.score2 && team === 1 ? teams[teamToUpdate].loses + 1 : teams[teamToUpdate].loses;
+  teams[teamToUpdate].goals_for = team === 1 ? teams[teamToUpdate].goals_for + match.score1 : teams[teamToUpdate].goals_for + match.score2;
+  teams[teamToUpdate].goals_against = team === 1 ?
+    teams[teamToUpdate].goals_against + match.score2 : teams[teamToUpdate].goals_against + match.score1;
+  teams[teamToUpdate].goal_difference = team === 1 ?
+    teams[teamToUpdate].goal_difference + (match.score1 - match.score2) : teams[teamToUpdate].goal_difference + (match.score2 - match.score1);
+  teams[teamToUpdate].points = match.score1 !== match.score2 ?
+    (match.score1 > match.score2 && team === 1) || (match.score1 < match.score2 && team === 2) ?
+      teams[teamToUpdate].points + 3 :
+        teams[teamToUpdate].points :
+          teams[teamToUpdate].points + 1;
+}
+
+function sortTable() {
+  teams.sort((a, b) => {
+    const sort = a.points === b.points ?
+      a.goal_difference === b.goal_difference ?
+        b.goals_for - a.goals_for :
+          b.goal_difference - a.goal_difference :
+            b.points - a.points;
+    return sort;
+  });
+}
+
+function addRankToTable() {
+  teams.map(
+    (team, index) => team.rank = index + 1
+  );
 }
 
 function buildTableFromResults(results) {
-  const teams = {};
-
   results.rounds.forEach(
     round => round.matches.forEach(
       match => {
-        let team1 = teams[match.team1.key];
-        if (!team1) {
-          team1 = {
-            key: match.team1.key,
-            name: match.team1.name,
-            code: match.team1.code,
-            wins: match.score1 > match.score2 ?  1 : 0,
-            draws: match.score1 === match.score2 ? 1 : 0,
-            loses: match.score1 < match.score2 ? 1 : 0,
-            goals_for: match.score1,
-            goals_against: match.score2,
-            goal_difference: match.score1 - match.score2,
-            points: match.score1 === match.score2 ? 1 : match.score1 > match.score2 ? 3 : 0
-          };
-          teams[match.team1.key] = team1;
+        if (teams.filter(team => (team.key === match.team1.key)).length === 0) {
+          addTeamToTable(match, 1);
         } else {
-          team1.wins = match.score1 > match.score2 ? team1.wins + 1 : team1.wins;
-          team1.draws = match.score1 === match.score2 ? team1.draws + 1 : team1.draws;
-          team1.loses = match.score1 < match.score2 ? team1.loses + 1 : team1.loses;
-          team1.goals_for = team1.goals_for + match.score1;
-          team1.goals_against = team1.goals_against + match.score2;
-          team1.goal_difference = team1.goal_difference + (match.score1 - match.score2);
-          team1.points = match.score1 === match.score2 ? team1.points + 1 : match.score1 > match.score2 ? team1.points + 3 : team1.points;
+          updateTeam(match, match.team1.key, 1);
         }
 
-        let team2 = teams[match.team2.key];
-        if (!team2) {
-          team2 = {
-            key: match.team2.key,
-            name: match.team2.name,
-            code: match.team2.code,
-            wins: match.score2 > match.score1 ? 1 : 0,
-            draws: match.score2 === match.score1 ? 1 : 0,
-            loses: match.score2 < match.score1 ? 1 : 0,
-            goals_for: match.score2,
-            goals_against: match.score1,
-            goal_difference: match.score2 - match.score1,
-            points: match.score1 === match.score2 ? 1 : match.score2 > match.score1 ? 3 : 0
-          }
-          teams[match.team2.key] = team2;
+        if (teams.filter(team => (team.key === match.team2.key)).length === 0) {
+          addTeamToTable(match, 2);
         } else {
-          team2.wins = match.score2 > match.score1 ? team2.wins + 1 : team2.wins;
-          team2.draws = match.score2 === match.score1 ? team2.draws + 1 : team2.draws;
-          team2.loses = match.score2 < match.score1 ? team2.loses + 1 : team2.loses;
-          team2.goals_for = team2.goals_for + match.score2;
-          team2.goals_against = team2.goals_against + match.score1;
-          team2.goal_difference = team2.goal_difference + (match.score2 - match.score1);
-          team2.points = match.score2 === match.score1 ? team2.points + 1 : match.score2 > match.score1 ? team2.points + 3 : team2.points;
+          updateTeam(match, match.team2.key, 2);
         }
       }
     )
   )
+
+  sortTable();
+  addRankToTable();
   console.log(teams);
-  return sortTableOrder(teams);
+  return teams;
 }
 
-const results = buildTableFromResults(mockData);
+buildTableFromResults(data);
